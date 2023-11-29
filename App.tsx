3bce19d6830792d5ddc994797d42edebc01aa3ff
mrznaboya <zuyax@yahoo.com>
2023-11-29 20:06:36 +0800

    Added screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+
+import {
+  DrawerContentScrollView,
+  createDrawerNavigator,
+} from "@react-navigation/drawer";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import {
+  GluestackUIProvider,
+  HStack,
+  Icon,
+  Pressable,
+  Text,
+  VStack,
+} from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import CreatorScreen from "./components/CreatorScreen";
+import AboutScreen from "./components/AboutScreen";
+import HomeScreen from "./components/HomeScreen";
+
+const Drawer = createDrawerNavigator();
+
+export const CustomDrawerContent = (props: any) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <VStack>
+        {props.state.routeNames.map((name: string, index: number) => (
+          <Pressable onPress={(event) => props.navigation.navigate(name)}>
+            <HStack>
+              <Icon
+                size="lg"
+                color={
+                  index === props.state.index ? "$rose600" : "$blueGray700"
+                }
+                as={<MaterialCommunityIcons name="home" />}
+              ></Icon>
+              <Text
+                fontWeight="$medium"
+                color={
+                  index === props.state.index ? "$rose600" : "$blueGray700"
+                }
+              >
+                {name}
+              </Text>
+            </HStack>
+          </Pressable>
+        ))}
+      </VStack>
+    </DrawerContentScrollView>
+  );
+};
 
 export default function App() {
+  const headerStyle = {
+    headerStyle: {
+      backgroundColor: config.tokens.colors.rose600,
+    },
+    headerTintColor: "#FFF",
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Drawer.Navigator
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            initialRouteName="Creator"
+          >
+            <Drawer.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                title: "Trending Memes",
+                ...headerStyle,
+              }}
+            />
+            <Drawer.Screen
+              name="Creator"
+              component={CreatorScreen}
+              options={{
+                title: "Meme Generator",
+                ...headerStyle,
+              }}
+            />
+            <Drawer.Screen
+              name="About"
+              component={AboutScreen}
+              options={{
+                title: "About the App",
+                ...headerStyle,
+              }}
+            />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/AboutScreen.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+import React from "react";
+import { Center } from "@gluestack-ui/themed";
+import { Heading } from "@gluestack-ui/themed";
+
+const AboutScreen = () => {
+  return (
+    <View>
+      <Center>
+        <Heading>About</Heading>
+      </Center>
+    </View>
+  );
+};
+
+export default AboutScreen;
